16dff465c388efaa1f427f7ab73d26a88f24bbb9
Gepoverlow <gepeoverload@gmail.com>
2025-09-25 15:03:44 +0200

    finished day 3 part 2

--- days/day-3.py
+++ days/day-3.py
@@ -1,9 +1,9 @@
+import math
+
 with open('./inputs/day-3.txt') as file:
     lines = [line.strip() for line in file]
 
-def solve_day_three_part_one():
-    r = 3
-    d = 1
+def solve_day_three_part_one(r, d):
     x, y = 0, 0
     count = 0
 
@@ -16,6 +16,21 @@ def solve_day_three_part_one():
         if y >= len(lines):
             break
     
-    print(count)
+    return count
 
-solve_day_three_part_one()
+def solve_day_three_part_two():
+    slopes = [
+        [1, 1],
+        [3, 1],
+        [5, 1],
+        [7, 1],
+        [1, 2]
+    ]
+    tree_counts = [solve_day_three_part_one(r, d) for r, d in slopes]
+    result = math.prod(tree_counts)
+
+    print("result third day part two:", result)
+
+
+print("result third day part one:", solve_day_three_part_one(3, 1))
+solve_day_three_part_two()
