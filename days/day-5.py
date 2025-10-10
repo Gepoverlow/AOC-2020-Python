f113e530ad01b785c5cf48336425dea15d4da566
Gepoverlow <gepeoverload@gmail.com>
2025-10-10 15:16:55 +0200

    finished day 5 part 2

--- days/day-5.py
+++ days/day-5.py
@@ -13,19 +13,37 @@ def binary_partition(boarding_code, low, high, lower_char, upper_char):
 
     if(boarding_letter == upper_char):
         return binary_partition(boarding_code[1:], mid + 1, high, lower_char, upper_char)
+    
+def get_seat_id(line, total_rows=127, total_columns=7):
+    row = binary_partition(line[:7], 0, total_rows, 'F', 'B')
+    column = binary_partition(line[7:], 0, total_columns, 'L', 'R')
+    return row * 8 + column
 
 def solve_day_five_part_one():
-    total_rows = 127
-    total_columns = 7
     highest_id = 0
 
     for line in lines:
-        row = binary_partition(line[0:7], 0, total_rows, 'F', 'B')
-        column = binary_partition(line[7:10], 0, total_columns, 'L', 'R')
-        seat_id = (row * 8 + column)
+        seat_id = get_seat_id(line)
         if seat_id > highest_id:
             highest_id = seat_id
 
-    print('Highest seat id:', highest_id)
+    print('Highest seat ID:', highest_id)
+
+def solve_day_five_part_two():
+    seat_ids = []
+
+    for line in lines:
+        seat_ids.append(get_seat_id(line))
+
+    seat_ids.sort()
+
+    my_seat = None
+    for i in range(len(seat_ids) - 1):
+        if seat_ids[i + 1] != seat_ids[i] + 1:
+            my_seat = seat_ids[i] + 1
+            break
+
+    print('My seat ID:', my_seat)
 
 solve_day_five_part_one()
+solve_day_five_part_two()
