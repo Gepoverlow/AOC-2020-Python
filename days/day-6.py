740e8dd4103ec28dd115996285b851e2ccd7366e
Gepoverlow <gepeoverload@gmail.com>
2025-10-23 17:37:05 +0200

    finish day 6 part 2

--- days/day-6.py
+++ days/day-6.py
@@ -5,7 +5,7 @@ with open('./inputs/day-6.txt') as file:
 
 groups = [list(g) for k, g in groupby(lines, key=bool) if k]
 
-def get_group_count(group):
+def get_group_count_day_one(group):
     unique_questions = set()
 
     for person in group:
@@ -14,15 +14,33 @@ def get_group_count(group):
 
     return len(unique_questions)
 
+def get_group_count_day_two(group):
+    common_questions = set(group[0])
+
+    for person in group[1:]:
+        common_questions = common_questions.intersection(set(person))
+
+    return len(common_questions)
+
 def solve_day_six_part_one():
     result = 0
 
     for group in groups:
-        count = get_group_count(group)
+        count = get_group_count_day_one(group)
 
         result = result + count
 
     print('Total count:', result)
 
+def solve_day_six_part_two():
+    result = 0
+
+    for group in groups:
+        count = get_group_count_day_two(group)
+
+        result = result + count
+
+    print('Total count:', result)
 
 solve_day_six_part_one()
+solve_day_six_part_two()
